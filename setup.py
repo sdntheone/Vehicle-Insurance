8c53f4e345c1412bac33378b079195e11547aae9
SUDHANSHU NANDAN <sdntheone@gmail.com>
2025-05-28 04:36:49 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sudhanshu Nandan",
+    author_email="sdntheone@gmail.com",
+    packages=find_packages()
+)
